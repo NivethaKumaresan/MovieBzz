e8e342e935570fdce60b11a6bba0f5a181acb908
NivethaKumaresan <153165507+NivethaKumaresan@users.noreply.github.com>
2025-02-28 12:48:42 +0530

    Add files via upload

---
+++ MovieBuzz.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class MovieBuzz 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        while (true) 
+        {
+            System.out.print("Enter Your Hero: ");
+            
+            String hero = scanner.nextLine().toLowerCase();
+            if (hero.equals("exit")) 
+            {
+                System.out.println("Exiting MovieBuzz.");
+                break;
+            }
+            switch (hero) 
+            {
+                
+                case "bala":
+                    System.out.println("bala 1");
+                    System.out.println("bala 2");
+                    System.out.println("bala 3");
+                    break;
+                case "chiru":
+                    System.out.println("chiru 1");
+                    System.out.println("chiru 2");
+                    System.out.println("chiru 3");
+                    System.out.println("chiru 4");
+                    break;
+                case "balaya":
+                    System.out.println("bala 1");
+                    System.out.println("bala 2");
+                    System.out.println("bala 3");
+                    break;
+                default:
+                    System.out.println("Sorry, no movies found for the hero: " + hero);
+                    break;
+            }
+
+            System.out.println(); 
+        }
+
+        scanner.close();
+    }
+}
+
+
