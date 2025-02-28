cf53514a97bc6d5a7c816b7cc74a8cec0499a02e
NivethaKumaresan <153165507+NivethaKumaresan@users.noreply.github.com>
2025-02-28 13:03:13 +0530

    Update MovieBuzz.java

--- MovieBuzz.java
+++ MovieBuzz.java
@@ -50,3 +50,29 @@ public class MovieBuzz
 }
 
 
+
+
+// OUTPUT:
+// Enter Your Hero: movie
+// bala
+// balaya
+// chiru
+
+// Enter Your Hero: bala
+// bala 1
+// bala 2
+// bala 3
+
+// Enter Your Hero: chiru
+// chiru 1
+// chiru 2
+
+// Enter Your Hero: balaya
+// bala 1
+// bala 2
+// bala 3
+
+// Enter Your Hero: exit
+// Exiting MovieBuzz.
+
+
